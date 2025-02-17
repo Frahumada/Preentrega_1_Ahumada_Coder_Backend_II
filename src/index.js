b49909443ea4692edd1900bcf54b6337df409700
Rodrigo Ceballos <frahumada.dev@gmail.com>
2025-02-16 22:17:56 -0300

    commit con session en mongo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import express from "express";
+import session from "express-session";
+import FileStorage from "session-file-store";
 import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
 
 //settings
 const app = express();
 app.set("PORT", 3000);
-const firmaCookie = "Carlos Tevez"
+const firmaCookie = "Carlos Tevez";
+// const fileStorage = FileStorage(session)
+const secret = "Apache3210";
+const mongodbUri = "mongodb+srv://Frahumada_Admin:UyiKMmsUH7TO9QrM@frahumada.twiqt.mongodb.net/?retryWrites=true&w=majority&appName=Frahumada";
+
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(firmaCookie)); //
+// Parte 1 --> FileStorage
+// app.use(session({
+//   store: new fileStorage({path:"./sessions",ttl:10, retries:0}),
+//   secret,
+//   resave: false,
+//   saveUninitialized: false,
+// }))
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: mongodbUri,
+      ttl: 15,
+    }),
+    secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //routes
 app.get("/", (req, res) => {
@@ -18,7 +43,7 @@ app.get("/", (req, res) => {
 
 // routes cookies
 app.get("/set-cookie", (req, res) => {
-  res.cookie("MyCookie", "Valor de la cookie", {maxAge: 10000});
+  res.cookie("MyCookie", "Valor de la cookie", { maxAge: 10000 });
   res.send("Cookie seteada");
 });
 
@@ -35,7 +60,7 @@ app.get("/delete-cookie", (req, res) => {
 
 // routes cookies firmadas
 app.get("/set-signed-cookie", (req, res) => {
-  res.cookie("MySignedCookie", "Valor de la cookie FIRMADA", {signed: true});
+  res.cookie("MySignedCookie", "Valor de la cookie FIRMADA", { signed: true });
   res.send("Cookie FIRMADA seteada");
 });
 
@@ -50,8 +75,26 @@ app.get("/delete-signed-cookie", (req, res) => {
   res.send("Cookie FIRMADA removida");
 });
 
+//login
+app.get("/login", (req, res) => {
+  const user = req.query.user;
+  const pass = req.query.pass;
+
+  req.session.user = user;
+  res.redirect("/user");
+});
+
+app.get("/user", (req, res) => {
+  const user = req.session.user;
+  if (req.session?.user) {
+    return res.send(`El usuario registrado es ${user}`);
+  }
+  {
+    res.send(`No hay usuario registrado`);
+  }
+});
+
 //listeners
 app.listen(app.get("PORT"), () => {
   console.log(`🟢 Server on port http://localhost:${app.get("PORT")} 🟢`);
 });
-
